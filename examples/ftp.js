1b7a44e5a87743c9a1b2a7ecdb80cdbe0a5ac7cb
Mike Grabowski <grabbou@gmail.com>
2014-08-19 16:16:13 +0200

    Added FTP simple example + method

---
+++ examples/ftp.js
@@ -0,0 +1,18 @@
+// Load env variables
+require('dotenv').load();
+
+// Simple initialization
+var Storage = require('../lib');
+
+Storage.init({
+	ftp: {
+		host: process.env.TEST_FTP_HOSTNAME,
+		user: process.env.TEST_FTP_USERNAME,
+		pass: process.env.TEST_FTP_PASSWORD,
+		path: process.env.TEST_FTP_PATH,
+		port: 22
+	}
+});
+
+// Because no process.env.storage is specified, we use 'amazon' for now
+var ftp = Storage.obtain('ftp');
