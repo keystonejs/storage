23f807a0e380163841f0ec9e77faa12bb588d3d6
Mike Grabowski <grabbou@gmail.com>
2014-08-31 00:42:01 +0200

    Test refactoring part 2

--- test/storage_test.js
+++ test/storage_test.js
@@ -16,10 +16,18 @@ var expect = require('chai').expect,
 describe('Storage', function () {
 
 	var Storage = require('../lib'),
-		_init = stub(Storage.Providers.AmazonS3.prototype, '_init').callsArgWith(0, null);
+		_init = stub(Storage.Providers.AmazonS3.prototype, '_init').callsArgWith(0, null),
+		_cachedInstance = spy(Storage.Providers, 'AmazonS3');
 
 	beforeEach(function () {
-		Storage.init({});
+		Storage.init({
+			amazon: {
+				provider: Storage.Providers.AmazonS3,
+				key: '',
+				keyId: '',
+				container: ''
+			}
+		});
 		Storage._cache = {};
 	});
 
@@ -52,96 +60,66 @@ describe('Storage', function () {
 	describe('#_getInstance', function () {
 
 		it('should throw an error if instance is missing a config', function () {
+			delete Storage._config.amazon;
 			expect(function () {
-				Storage.get('amazon');
+				Storage._getInstance('amazon');
 			}).to.throw(/Configuration for amazon is missing/);
 		});
 
 		it('should throw an error if no provider set for an instance', function () {
-			Storage.init({
-				amazon: {}
-			});
-
+			delete Storage._config.amazon.provider;
 			expect(function () {
-				Storage.get('amazon');
+				Storage._getInstance('amazon');
 			}).to.throw(/Provider for amazon is not specified/);
 		});
 
 		it('should cache instances', function () {
-			var constructor = spy(Storage.Providers, 'AmazonS3');
-
-			Storage.init({
-				amazon: {
-					provider: Storage.Providers.AmazonS3,
-					key: '',
-					keyId: '',
-					container: ''
-				}
-			});
-
 			Storage._getInstance('amazon');
 			Storage._getInstance('amazon');
-
-			expect(constructor.calledOnce).to.be.true;
+			expect(_cachedInstance.calledOnce).to.be.true;
 		});
 
 	});
 
-	describe('', function () {
+	describe('#get', function () {
 
-		beforeEach(function () {
-			Storage.init({
-				amazon: {
-					provider: Storage.Providers.AmazonS3,
-					key: '',
-					keyId: '',
-					container: ''
-				}
+		it('should call _init on every get', function () {
+			Storage.get('amazon', function () {});
+			Storage.get('amazon', function () {});
+			expect(_init.calledTwice).to.be.true;
+		});
+
+		it.skip('should assign default instance if null', function (next) {
+			Storage.get(function (err, client) {
+				expect(client).to.be.an.instanceof(Storage.Providers.AmazonS3);
+				next();
 			});
 		});
 
-		describe('#get', function () {
+	});
 
-			it('should call _init on every get', function () {
-				Storage.get('amazon', function () {});
-				Storage.get('amazon', function () {});
-				expect(_init.calledTwice).to.be.true;
-			});
+	describe('#pre', function () {
 
-			it.skip('should assign default instance if null', function (next) {
-				Storage.get(function (err, client) {
-					expect(client).to.be.an.instanceof(Storage.Providers.AmazonS3);
-					next();
-				});
-			});
+		it.skip('should set local hook', function () {
 
 		});
 
-		describe('#pre', function () {
-
-			it.skip('should set local hook', function () {
-
-			});
-
-			it.skip('should set global hook', function () {
-
-			});
-
-		});
-
-		describe('#post', function () {
-
-			it.skip('should set local hook', function () {
-
-			});
-
-			it.skip('should set global hook', function () {
-
-			});
+		it.skip('should set global hook', function () {
 
 		});
 
 	});
 
+	describe('#post', function () {
+
+		it.skip('should set local hook', function () {
+
+		});
+
+		it.skip('should set global hook', function () {
+
+		});
+
+	});
 
 });
