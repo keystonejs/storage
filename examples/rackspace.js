5fccf5f8a41a9380f69d770c15e8705dd51a09b4
Mike Grabowski <grabbou@gmail.com>
2014-09-04 19:32:10 -0400

    Fixes #10

---
+++ examples/rackspace.js
@@ -0,0 +1,31 @@
+// Load env variables
+require('dotenv').load();
+
+// Simple initialization
+var Storage = require('../lib');
+
+Storage.init({
+	rackspace: {
+		provider: Storage.Providers.Rackspace,
+		container: process.env.RACKSPACE_CONTAINER,
+		apiKey: process.env.RACKSPACE_TOKEN,
+		username: process.env.RACKSPACE_USERNAME,
+		region: process.env.RACKSPACE_REGION
+	}
+});
+
+Storage.get('rackspace', function (err, client) {
+
+	if (err) console.log(err);
+
+	// As long as not implemented, will raise an error here
+	client.upload('../LICENSE', 'license.md', function (err) {
+		if (err) {
+			console.log(err.message);
+		} else {
+			console.log('Uploaded');
+		}
+	});
+
+});
+
